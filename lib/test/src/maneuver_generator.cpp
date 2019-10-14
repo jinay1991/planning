c407da44cc57c635e9b20c9a62e75c60d2eada13
Jinay Patel <jinay1991@gmail.com>
2019-10-14 19:06:35 +0200

    Updated Unit Test

--- lib/test/src/maneuver_generator.cpp
+++ lib/test/src/maneuver_generator.cpp
@@ -12,8 +12,8 @@ namespace
 {
 TEST(ManeuverGeneratorSpecTest, GeneratesThreeManeuvers)
 {
-    auto maneuver_generator = ManeuverGenerator{};
-    auto maneuvers = maneuver_generator.Generate(units::velocity::meters_per_second_t{17.0});
+    const auto target_velocity = units::velocity::meters_per_second_t{17.0};
+    const auto maneuvers = ManeuverGenerator().Generate(target_velocity);
     ASSERT_THAT(maneuvers.size(), ::testing::Eq(3U));
 }
 
