a3dfee912307efee715d55c5a2bcad679bf74544
Jinay Patel <jinay1991@gmail.com>
2019-12-14 13:41:13 +0100

    Fix Clang-Format

--- lib/test/src/trajectory_optimizer_test.cpp
+++ lib/test/src/trajectory_optimizer_test.cpp
@@ -7,7 +7,6 @@
 
 #include "motion_planning/trajectory_optimizer.h"
 
-
 #include "data_source_builder.h"
 #include "trajectory_builder.h"
 
@@ -31,18 +30,18 @@ TEST_F(TrajectoryOptimizerSpec, GivenPlannedTrajectories_WhenEvaluated_ThenRetur
     const auto start_yaw = units::angle::radian_t{0.0};
     const auto displacement = units::length::meter_t{30.0};
     const std::int32_t count = 30;
-    const auto trajectories = Trajectories{TrajectoryBuilder().
-                                           WithLaneId(LaneId::kEgo).
-                                           WithWaypoints(start_position, start_yaw, count, displacement).
-                                           Build()};
+    const auto trajectories =
+        Trajectories{TrajectoryBuilder().WithWaypoints(start_position, start_yaw, count, displacement).Build()};
     auto data_source =
         DataSourceBuilder().WithPreviousPath(PreviousPathGlobal{}).WithMapCoordinates(map_waypoints_).Build();
 
     const auto actual = TrajectoryOptimizer(data_source).GetOptimizedTrajectories(trajectories);
 
     EXPECT_EQ(actual.size(), trajectories.size());
-    // EXPECT_EQ(actual[0].global_lane_id, GlobalLaneId::kInvalid);
+    EXPECT_EQ(actual[0].global_lane_id, GlobalLaneId::kCenter);
 }
+
+/// @todo Add Test confirming the curve for Trajectory is smooth enough
 
 }  // namespace
 }  // namespace motion_planning
